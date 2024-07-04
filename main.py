8100175f7e2fcd74c079f9bc9b7d773d205cf20c
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2024-07-04 13:00:08 +0530

    done

--- main.py
+++ main.py
@@ -12,8 +12,9 @@ from sklearn.metrics import accuracy_score
 st.title('ML DATASET COMPARISON')
 
 st.write("""
- Discover the ultimate machine learning model for your dataset! Dive into our interactive tool to compare top classifiers and see which one reigns supreme
- Compare top classifiers across diverse datasets to find the best performer for your needs
+ Discover the ultimate machine learning model for your dataset! Dive into our interactive tool to compare top classifiers and see which one reigns supreme.
+ 
+Compare top classifiers across diverse datasets to find the best performer for your needs
 """)
 
 dataset_name = st.sidebar.selectbox('Select Dataset', ('Iris', 'Breast Cancer', 'Wine'))
